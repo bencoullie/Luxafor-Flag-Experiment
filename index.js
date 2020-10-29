b09a50f4ed534da983563cb5340110d64a27a55a
Benjamin Coullie <ben.coullie@vendhq.com>
2020-10-29 15:59:44 +1300

    initial script for monitoring media completion

---
+++ index.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+
+// Using luxafor-api package to interface with hardware
+const { device } = require('luxafor-api')
+
+// Connect to the first available (connected) flag
+const luxaforFlag = device()
+
+const watchMedia = () => {
+	setInterval(async () => {
+		// eslint-disable-next-line no-console
+		console.log('Fetching')
+		const response = await fetch('https://oneyearonemediaserver.herokuapp.com/api/profiles')
+		const data = await response.json()
+		const myMedia = data.filter(profile => profile.consumer_name === 'Benc')
+		const completedMedia = myMedia.every(item => item.completed)
+
+		if (completedMedia) {
+			luxaforFlag.color('green')
+		} else {
+			luxaforFlag.color('red')
+		}
+	}, 1000)
+}
+
+watchMedia()
